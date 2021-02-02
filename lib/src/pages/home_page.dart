57f038ca4e16987856cbcb5b94d19d10cde3aa16
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-01 21:32:30 -0700

    Main Swiper Cards and Footer

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:movies/src/providers/movies_provider.dart';
 import 'package:movies/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
+  final moviesProvider = new MoviesProvider();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,15 +16,54 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: Column(
-          children: <Widget>[_swiperCards()],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[_swiperCards(), _footer(context)],
         ),
       ),
     );
   }
 
   Widget _swiperCards() {
-    return CardSwiper(
-      movies: [1, 2, 3, 4, 5],
+    return FutureBuilder(
+      future: moviesProvider.getNowPlaying(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return CardSwiper(
+            movies: snapshot.data,
+            height: 300,
+          );
+        } else {
+          return Container(
+              height: 400, child: Center(child: CircularProgressIndicator()));
+        }
+      },
     );
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Text(
+              'Populars',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            FutureBuilder(
+                future: moviesProvider.getPopulars(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return CardSwiper(
+                      movies: snapshot.data,
+                      height: 100,
+                    );
+                  } else {
+                    return Container(
+                        height: 100,
+                        child: Center(child: CircularProgressIndicator()));
+                  }
+                })
+          ],
+        ));
   }
 }
