31eab93b3fcef38ac22f0d7460ed2273950edf2b
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-01 17:41:57 -0700

    HomePage Swiper implemented

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Billboard Movies'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
+      body: Container(
+        child: Column(
+          children: [_swiperCards()],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperCards() {
+    return Container(
+      padding: EdgeInsets.only(top: 10),
+      width: double.infinity,
+      height: 300,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+        layout: SwiperLayout.STACK,
+        itemWidth: 300,
+      ),
+    );
+  }
+}
