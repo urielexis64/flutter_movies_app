dee9367c3a09a196c02adc58ff5c17c8f925a0d6
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-01 20:51:27 -0700

    Movie model

---
+++ lib/src/models/movie_model.dart
@@ -0,0 +1,65 @@
+class Movies {
+  List<Movie> items = new List();
+
+  Movies();
+
+  Movies.fromJSONList(List<dynamic> jsonList) {
+    if (jsonList == null) return;
+
+    for (final item in jsonList) {
+      final movie = new Movie.fromJSONMap(item);
+      items.add(movie);
+    }
+  }
+}
+
+class Movie {
+  bool adult;
+  String backdropPath;
+  List<int> genreIds;
+  int id;
+  String originalLanguage;
+  String originalTitle;
+  String overview;
+  double popularity;
+  String posterPath;
+  String releaseDate;
+  String title;
+  bool video;
+  double voteAverage;
+  int voteCount;
+
+  Movie({
+    this.adult,
+    this.backdropPath,
+    this.genreIds,
+    this.id,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    this.title,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+
+  Movie.fromJSONMap(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    genreIds = json['genre_ids'].cast<int>();
+    id = json['id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'] / 1;
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'] / 1;
+    voteCount = json['vote_count'];
+  }
+}
