767b17d0313f7e168d39b56691901c46ce0a3075
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-02 16:24:13 -0700

    Bloc Pattern implemented

--- lib/src/providers/movies_provider.dart
+++ lib/src/providers/movies_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
@@ -7,6 +8,21 @@ class MoviesProvider {
   String _apiKey = 'd278c593d23e539bae7d6db38e2cbebd';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  int _popularsPage = 0;
+  bool _loading = false;
+
+  List<Movie> _populars = List();
+
+  final _popularsStreamController = StreamController<List<Movie>>.broadcast();
+
+  Function(List<Movie>) get popularsSink => _popularsStreamController.sink.add;
+
+  Stream<List<Movie>> get popularsStream => _popularsStreamController.stream;
+
+  void disposeStreams() {
+    _popularsStreamController.close();
+  }
 
   Future<List<Movie>> _processResponse(Uri url) async {
     final response = await http.get(url);
@@ -24,8 +40,24 @@ class MoviesProvider {
   }
 
   Future<List<Movie>> getPopulars() async {
-    final url = Uri.https(
-        _url, '3/movie/popular', {'api_key': _apiKey, 'language': _language});
-    return await _processResponse(url);
+    if (_loading) return [];
+
+    _loading = true;
+
+    _popularsPage++;
+
+    print(_popularsPage);
+
+    final url = Uri.https(_url, '3/movie/popular',
+        {'api_key': _apiKey, 'language': _language, 'page': '$_popularsPage'});
+
+    final response = await _processResponse(url);
+
+    _populars.addAll(response);
+    popularsSink(_populars);
+
+    _loading = false;
+
+    return response;
   }
 }
