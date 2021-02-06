fe64c3789dfbedf331a5e3c716a00b42cab68736
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-05 21:53:20 -0700

    Actors model

---
+++ lib/src/models/actors_model.dart
@@ -0,0 +1,72 @@
+class Cast {
+  List<Actor> actors = new List();
+
+  Cast();
+
+  Cast.fromJSONList(List<dynamic> jsonList) {
+    if (jsonList == null) return;
+
+    jsonList.forEach((element) {
+      final actor = new Actor.fromJSONMap(element);
+      actors.add(actor);
+    });
+  }
+}
+
+class Actor {
+  bool adult;
+  int gender;
+  int id;
+  String knownForDepartment;
+  String name;
+  String originalName;
+  double popularity;
+  String profilePath;
+  int castId;
+  String character;
+  String creditId;
+  int order;
+  String department;
+  String job;
+
+  Actor({
+    this.adult,
+    this.gender,
+    this.id,
+    this.knownForDepartment,
+    this.name,
+    this.originalName,
+    this.popularity,
+    this.profilePath,
+    this.castId,
+    this.character,
+    this.creditId,
+    this.order,
+    this.department,
+    this.job,
+  });
+
+  Actor.fromJSONMap(Map<String, dynamic> json) {
+    adult = json['adult'];
+    gender = json['gender'];
+    id = json['id'];
+    knownForDepartment = json['known_for_department'];
+    name = json['name'];
+    originalName = json['original_name'];
+    popularity = json['popularity'];
+    profilePath = json['profile_path'];
+    castId = json['castId'];
+    character = json['character'];
+    creditId = json['credit_id'];
+    order = json['order'];
+    department = json['department'];
+    job = json['job'];
+  }
+
+  String getImgPoster() {
+    if (profilePath == null) {
+      return 'https://www.adrcdoorcounty.org/wp-content/uploads/2019/05/mystery-person.jpg';
+    }
+    return 'https://image.tmdb.org/t/p/w500/$profilePath';
+  }
+}
