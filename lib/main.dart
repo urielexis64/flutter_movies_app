c7bdb029c14b095c557810140b7a6fdebc41bbdd
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-05 21:51:16 -0700

    detail route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movies/src/pages/home_page.dart';
+import 'package:movies/src/pages/movie_detail.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Movies App',
       initialRoute: '/',
-      routes: {'/': (BuildContext context) => HomePage()},
+      routes: {
+        '/': (BuildContext context) => HomePage(),
+        'detail': (BuildContext context) => MovieDetail()
+      },
     );
   }
 }
