19b25d52ed36dbde5c4688b79f2ed7bbe4fbd96f
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-06 20:43:34 -0700

    background text widget

---
+++ lib/src/widgets/background_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BackgroundText extends StatelessWidget {
+  final text;
+
+  BackgroundText({@required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: WidgetSpan(
+          child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: Color.fromRGBO(20, 20, 20, .8)),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontWeight: FontWeight.w500, color: Colors.white, fontSize: 13),
+          overflow: TextOverflow.ellipsis,
+        ),
+      )),
+    );
+  }
+}
